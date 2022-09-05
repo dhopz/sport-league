b7d955ffafa8e6f66d5ffb2ba351415716d7502f
dhopz <david_houpapa@hotmail.com>
2022-09-05 12:25:36 +0100

    generate table based on results

--- src/main/java/league/App.java
+++ src/main/java/league/App.java
@@ -14,6 +14,11 @@ public class App {
     private static final Integer number = 6;
 
     private static final List<String> completedGames = new ArrayList<>();
+    private static final List<int[]> games = new ArrayList<>();
+    private static final List<int[]> gamePoints = new ArrayList<>();
+    private static final List<int[]> resultsTable = new ArrayList<>();
+
+
 
 
     static {
@@ -27,11 +32,14 @@ public class App {
     public static void main(String[] args){
         logger.log(Level.INFO,"Hello World {0} ",randomScore());
 
-        System.out.println("Hello world " + randomScore() + " " + number);
-        System.out.println(createGames());
         gameResults();
+        calcPoints();
 
-        System.out.println(completedGames);
+        generateTable();
+        for (int[] game:resultsTable){
+            System.out.println(Arrays.toString(game));
+        }
+
 
     }
 
@@ -43,18 +51,8 @@ public class App {
         return IntStream.rangeClosed(0, 5).toArray();
     }
 
-    public static List<ArrayList<Integer>> createGames(){
-        ArrayList<Integer> results = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> games = new ArrayList<>();
-
-        results.add(1);
-        results.add(2);
-        results.add(3);
-        results.add(4);
-
-        games.add(results);
-
-        return games;
+    public static void createGames(int[] game){
+        games.add(game);
     }
 
     public static void completeFixtures(String fixture) {
@@ -70,36 +68,63 @@ public class App {
         return false;
     }
 
-
-
     public static void gameResults(){
         int[] teams = numberOfTeams();
-        System.out.println(Arrays.toString(teams));
-
-        int n = 5;
-        int [][] a = new int[n][];
-
 
         for (int homeTeam : teams) {
             for (int awayTeam : teams) {
                 if (Objects.equals(homeTeam, awayTeam)) continue;
                 if (!gameCompleted(String.valueOf(homeTeam), String.valueOf(awayTeam))) {
                     completeFixtures(String.valueOf(homeTeam) + String.valueOf(awayTeam));
-                    System.out.println(homeTeam + " " + awayTeam + " " + randomScore() + " " + randomScore());
+                    int[] game = {homeTeam,awayTeam,randomScore(),randomScore()};
+                    createGames(game);
                 }
             }
         }
+    }
+    public static void calcPoints(){
+        //[team,goalsfor,goalsagainst,goaldiff,points]
+        for (int[] game:games){
+            int homePoints;
+            int awayPoints;
+
+            if(game[2] > game[3]){
+                homePoints = 3;
+                awayPoints = 0;
+            } else if(game[2] < game[3]){
+                homePoints = 0;
+                awayPoints = 3;
+            } else {
+                homePoints =1;
+                awayPoints = 1;
+            }
+            gamePoints.add(new int[]{game[0],game[2],game[3],game[2]-game[3],homePoints});
+            gamePoints.add(new int[]{game[1],game[3],game[2],game[3]-game[2],awayPoints});
+
+        }
+
+    }
+
+    public static void generateTable(){
+        int[] teams = numberOfTeams();
+        for (int team:teams){
+            int goalsFor= 0;
+            int goalsAgainst= 0;
+            int goalDifference= 0;
+            int points= 0;
 
 
+            for (int[] result: gamePoints){
 
-//        int n = 5;
-//        int [][] a = new int[n][];
-
-//        for (int i=0; i<n; i++) {
-//            a[i] = new int[]{i,i+1,randomScore(),randomScore()};
-//        }
-
-        System.out.println(Arrays.deepToString(a));
+                if(Objects.equals(result[0], team)){
+                    points = points + result[4];
+                    goalsFor= goalsFor + result[1];
+                    goalsAgainst= goalsAgainst + result[2];
+                    goalDifference= goalDifference + result[3];
+                }
+            }
+            resultsTable.add(new int[] {team,goalsFor,goalsAgainst,goalDifference,points});
+        }
     }
 
 }
